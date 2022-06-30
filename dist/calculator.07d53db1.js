aa7e9e25df9fad853a42f35a0c594e2c757297ee
ravengaspar <ravengaspar@gmail.com>
2022-06-30 12:11:01 -0400

    fixed mobile full sizes

--- dist/calculator.07d53db1.js
+++ dist/calculator.07d53db1.js
@@ -295,7 +295,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39337" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37175" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
