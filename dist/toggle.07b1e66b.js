0132be3429cdc0cf53a320c1193b3a1a3d05e201
ravengaspar <ravengaspar@gmail.com>
2022-08-01 18:50:49 -0400

    fix the output size

--- dist/toggle.07b1e66b.js
+++ dist/toggle.07b1e66b.js
@@ -230,7 +230,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43405" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33693" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
